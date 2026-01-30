c23e528b04b638902dd2a9d62b640617d9dd6a53
hc <3558576544@qq.com>
2026-01-30 10:51:53 +0800

    强制使用UTF8保证终端输出不会乱码

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ from app.core import MainWindow, setup_di_container
 from app.core.util import load_components
 from app.core.di.container import get_container
 from app.core.util.logger import setup_logger
+import sys
+import io
 
+print("[DEBUG] sys.executable:", sys.executable)
+if sys.stdout.encoding.lower() != 'utf-8':
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+if sys.stderr.encoding.lower() != 'utf-8':
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 def main():
     # 日志级别: DEBUG, INFO, WARNING, ERROR
     setup_logger("app", level="DEBUG")
